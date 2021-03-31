b551b20ac60f2cfb8297a1d90c0e16a9dcbd1def
Louis Dégardin <louis.degardin@live.com>
2021-03-31 19:47:48 +0200

    Balancing rare wild pokemons rate

--- data/wildPokemon/seafoamislandb4.asm
+++ data/wildPokemon/seafoamislandb4.asm
@@ -2,11 +2,11 @@ IslandMonsB4:
 	db $0A
     db 31,SLOWPOKE
     db 31,SHELLDER
-    db 29,KABUTO
+    db 29,HORSEA
     db 33,SHELLDER
     db 29,SLOWPOKE
     db 31,SEEL
-    db 30,KABUTO
+    db 31,KABUTO
     db 29,SEEL
     db 39,SLOWBRO
     db 32,GOLBAT
