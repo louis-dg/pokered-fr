a7ecdbfa12fcb01d5f65a2bca8b484c0cc245004
Louis Dégardin <louis.degardin@live.com>
2023-08-16 17:32:54 +0200

    update name

--- documentation/doc_generator.py
+++ documentation/doc_generator.py
@@ -333,8 +333,8 @@ lienSection5 = "glossaire"
 
 docFile = open(DOCUMENTATION_FILE, 'w')
 docFile.write("# Pokémon Rouge\n\n")
-docFile.write("Ceci est la documentation associée à la hack rom Pokémon Rouge créée par LePatator.\n\n")
-docFile.write("Page du projet : https://github.com/LePatator/pokered-fr\n\n")
+docFile.write("Ceci est la documentation associée à la hack rom Pokémon Rouge créée par louis-dg.\n\n")
+docFile.write("Page du projet : https://github.com/louis-dg/pokered-fr\n\n")
 docFile.write("## Sommaire\n\n")
 docFile.write("- [" + titleSection1 + "](#" + lienSection1 + ")\n\n")
 docFile.write("- [" + titleSection2 + "](#" + lienSection2 + ")\n\n")
